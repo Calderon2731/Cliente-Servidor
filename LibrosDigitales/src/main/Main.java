af991e86d4ad627fe152fe8a4249c001a0176259
Kendall Calderon Acuña <calderonacunakendall@gmail.com>
2025-07-15 19:54:52 -0600

    vista

--- LibrosDigitales/src/main/Main.java
+++ LibrosDigitales/src/main/Main.java
@@ -1,9 +1,15 @@
 
 package main;
 
+import vista.Menu;
+
 public class Main {
 
     public static void main(String[] args) {
+        Menu m = new Menu();
+        m.setVisible(true);
+        m.setLocationRelativeTo(null);
+        
      
     }
     
